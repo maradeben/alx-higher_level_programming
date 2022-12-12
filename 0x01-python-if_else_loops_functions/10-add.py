0724ec29ca3f7838ada55e26cb566ce30c6c487c
Eben <maradebenezer@gmail.com>
2022-12-12 20:34:06 +0100

    Included solutions for 0x01

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+Returns the result of the sum of two integers
+"""
+
+
+def add(a, b):
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+calculates and return a to the power of b
+"""
+
+
+def pow(a, b):
+    return (a ** b)
