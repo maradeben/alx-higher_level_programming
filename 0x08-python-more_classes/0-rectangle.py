ebc3910c4e78e10d5d832f39f49340c7789826f4
Eben <maradebenezer@gmail.com>
2023-01-03 03:21:19 +0100

    Initial commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Rectangle Module
+Represents a rectangle object
+"""
+
+
+class Rectangle:
+    """ This is empty. It does nothing for now"""
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,6 @@
+# More on Classes
+Including:
+- Inheritance
+- Attribute amd Method Types
+- setter and getter methods
+- str() and repr() methods
