3bbb9131e3fd113d5ea3bafcd7c4a2bfe62ba7bd
Eben <maradebenezer@gmail.com>
2023-03-07 14:24:51 +0100

    fixed 6: s ;'

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,8 @@
 
 for i in range(0, 9):
     for j in range(1, 10):
+        if (i == j):
+            continue
         print("{:d}{:d}".format(i, j), end='')
         if not (i == 8 and j == 9):
             print(end=", ")
