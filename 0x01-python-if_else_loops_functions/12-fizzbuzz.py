9ee1d1e2365a5399e432a12da61b05eaad52cc74
Eben <maradebenezer@gmail.com>
2023-03-07 14:50:27 +0100

    fixed 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,12 +10,10 @@ print 'FizzBuzz' for number that are multiples of both
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0) and (i % 5 == 0):
-            print('FizzBuzz', end='')
+            print('FizzBuzz', end=' ')
         elif i % 3 == 0:
-            print('Fizz', end='')
+            print('Fizz', end=' ')
         elif i % 5 == 0:
-            print('Buzz', end='')
+            print('Buzz', end=' ')
         else:
-            print(i, end='')
-        if i < 100:
-            print(" ", end='')
+            print(i, end=' ')
