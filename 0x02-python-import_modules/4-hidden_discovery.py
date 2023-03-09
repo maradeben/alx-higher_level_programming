1f572b3908bb719d23a173954d6a2c2f3819116b
Eben <maradebenezer@gmail.com>
2023-03-09 20:34:25 +0100

    fixed no run on import in 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 
-sorted_list = []
-for name in dir("hidden_4.pyc"):
-    if not name.startswith("__"):
-        sorted_list.append(name)
-sorted_list.sort()
-for name in sorted_list:
-    print("{:s}".format(name))
+def main():
+    sorted_list = []
+    for name in dir("hidden_4.pyc"):
+        if not name.startswith("__"):
+            sorted_list.append(name)
+    sorted_list.sort()
+    for name in sorted_list:
+        print("{:s}".format(name))
+
+
+if (__name__ == "__main__"):
+    main()
