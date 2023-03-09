2745a063dc00057b9cd1cbf1c5eb82181aaf0686
Eben <maradebenezer@gmail.com>
2023-03-09 20:32:04 +0100

    fixed 2 and 4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,20 @@
 import sys
 
 
-args = sys.argv[1:]  # get arguments without filename
-num_args = len(args)
-if (num_args == 0):
-    print("{:d} arguments.".format(num_args))
-else:
-    if (num_args == 1):
-        title = "argument"
+def main():
+    args = sys.argv[1:]  # get arguments without filename
+    num_args = len(args)
+    if (num_args == 0):
+        print("{:d} arguments.".format(num_args))
     else:
-        title = "arguments"
-    print("{:d} {:s}:".format(num_args, title))
-    for idx, arg in enumerate(args):
-        print("{:d}: {:s}".format(idx + 1, arg))
+        if (num_args == 1):
+            title = "argument"
+        else:
+            title = "arguments"
+        print("{:d} {:s}:".format(num_args, title))
+        for idx, arg in enumerate(args):
+            print("{:d}: {:s}".format(idx + 1, arg))
+
+
+if (__name__ == "__main__"):
+    main()
