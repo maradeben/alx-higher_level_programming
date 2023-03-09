3363a98bda0cbb2d286762a94638f21ad0e91074
Eben <maradebenezer@gmail.com>
2023-03-09 21:56:45 +0100

    advanced

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,2 +1,11 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    from calculator_1 imp
+    from magic_calculation_102 import add, sub
+
+    if (a < 4):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, n)
+        return c
+    else:
+        return sub(a, b)
