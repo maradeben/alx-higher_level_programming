872d19ce3f9824ff8d7fdb835a99a6c70d271b07
Eben <maradebenezer@gmail.com>
2023-03-16 01:48:58 +0100

    fix 10 attempt

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 def best_score(a_dictionary):
 
+    if a_dictionary is None:
+        return (None)
     try:
         # sort by values and pick highest
         sort_dict = sorted(a_dictionary.items(), key=lambda x: x[1])
