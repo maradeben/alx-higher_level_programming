a806b4733e43a8cd21a0a7d7c9d1e50ba9d72436
Eben <maradebenezer@gmail.com>
2023-03-16 02:32:02 +0100

    completed advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+    if not my_list or my_list is None:
+        return 0
+    numerator = sum(list(map(lambda x: x[0] * x[1], my_list)))
+    denominator = sum([x[1] for x in my_list])
+
+    return (numerator / denominator)
