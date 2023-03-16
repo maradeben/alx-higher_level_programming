32f3be3ccb9ec1e40f1a99f09392b06fb391eaa0
Eben <maradebenezer@gmail.com>
2023-03-16 01:53:20 +0100

    fixed NameError in 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary is None or my_dict == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return (None)
     # sort by values and pick highest
     sort_dict = sorted(a_dictionary.items(), key=lambda x: x[1])
