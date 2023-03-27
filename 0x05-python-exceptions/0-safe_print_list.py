18f575526cdc51a8cc07b70cc62dd3be0e7ed80c
Eben <maradebenezer@gmail.com>
2023-03-27 22:30:25 +0100

    removed for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,14 +11,16 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         the number of arguments printed
     """
+    i = 0
 
     if x == 0:
         print()
-        return (0)
+        return (i)
 
-    for i in range(x):
+    while True:
         try:
             print(my_list[i], end='')
+            i+=1
         except IndexError:
             print('')
             return (i)
