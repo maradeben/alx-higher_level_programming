3c256317b54fbb85691e7898dc68b48f7526b724
Eben <maradebenezer@gmail.com>
2023-03-27 18:16:34 +0100

    fixing 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,9 @@ def safe_print_list(my_list=[], x=0):
     prints x items of a list
     x may be higher than number of elements in list
     """
+    if x == 0:
+        print()
+        return (0)
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
