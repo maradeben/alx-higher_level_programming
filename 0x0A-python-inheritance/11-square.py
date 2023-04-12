732658b830f21ec00a9b5869e232a3e5b137a34f
Eben <maradebenezer@gmail.com>
2023-04-12 14:19:36 +0100

    fixed checker errors

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,11 +10,12 @@ class Square(Rectangle):
     def __init__(self, size):
         """ initializing square class, overriding parent class' init"""
 
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
         return (self.__size ** 2)
-    
+
     def __str__(self):
         return ("[Square] {}/{}".format(self.__size, self.__size))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,4 +12,4 @@ def is_same_class(obj, a_class):
     Returns:
         True if isinstance, False otherwise
     """
-    return (isinstance(obj, a_class))
+    return (type(obj) == a_class)
