7bed583bed846e5c20fd40a427b47fa0f23f433b
Eben <maradebenezer@gmail.com>
2023-04-12 14:22:29 +0100

    fixed pycodestyle on 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,6 +12,6 @@ class Rectangle(BaseGeometry):
         """ initialize rectangle with width and height """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        
+
         self.__width = width
         self.__height = height
