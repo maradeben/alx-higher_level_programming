bd7d8aa67dd7f9dbff8316c776f3f80681ee6b81
Eben <maradebenezer@gmail.com>
2023-05-19 00:40:21 +0100

    initial commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create new user, set password and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON PRIVILEGES *.*
+TO 'user_0d_1'@'localhost';
