e46c4d25a283f4a351f0a063c7f7f5fdbc589e6b
Eben <maradebenezer@gmail.com>
2023-05-19 00:59:29 +0100

    fixed 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create new user, set password and grant all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON PRIVILEGES *.*
+GRANT ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost';
