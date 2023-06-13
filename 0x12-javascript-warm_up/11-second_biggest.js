150662d5a6dcf8c149ae52adcd683c0c3315952a
Eben <maradebenezer@gmail.com>
2023-06-13 17:09:31 +0100

    fixing 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,10 @@ const argv = process.argv.slice(2);
 if (!(argv.length > 1)) {
   console.log(0);
 } else {
+  // convert to numbers
+  const numbers = [];
+  for (const element of argv) {
+    numbers.push(parseInt(element));
+  }
   console.log(numbers.sort().reverse()[1]);
 }
