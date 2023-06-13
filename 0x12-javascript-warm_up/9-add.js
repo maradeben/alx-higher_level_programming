450319413028b11863438c81c568f2699553ef3a
Eben <maradebenezer@gmail.com>
2023-06-13 16:09:54 +0100

    added 10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 
+function add (a, b) {
+  return (parseInt(a) + parseInt(b));
+}
+
 const a = process.argv[2];
 const b = process.argv[3];
 
 if (isNaN(a) || isNaN(b)) {
   console.log('NaN');
 } else {
-  console.log(parseInt(a) + parseInt(b));
+  console.log(add(a, b));
 }
