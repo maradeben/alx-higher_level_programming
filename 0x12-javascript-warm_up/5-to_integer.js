1b3c91f5a6bc45effa8dcb29444b47bf28f000eb
Eben <maradebenezer@gmail.com>
2023-06-13 15:27:00 +0100

    fixed 5, removed extra space in console.log

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@ const process = require('process');
 const argv = process.argv;
 const arg = argv[2];
 
-if (isNaN(parseInt(arg))) {
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(arg));
+  console.log('My number:', parseInt(arg));
 }
