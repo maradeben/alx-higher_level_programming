a0a5a52f02988911f50e712d0bf779772349117f
Eben <maradebenezer@gmail.com>
2023-06-13 15:55:46 +0100

    added 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const a = process.argv[2];
+const b = process.argv[3];
+
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(parseInt(a) + parseInt(b));
+}
