658af1f8bdc99d003d547d0cb2a2587d6e7ed931
Eben <maradebenezer@gmail.com>
2023-06-30 15:57:21 +0100

    added 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,3 +4,12 @@ import requests
 import sys
 
 
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(owner, repo))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
