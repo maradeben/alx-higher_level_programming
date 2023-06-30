271b9daba290e41154f63f76f54c6b8abe957d7c
Eben <maradebenezer@gmail.com>
2023-06-30 13:35:43 +0100

    fixing 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,13 @@
 import urllib.request
 
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    resp = response.read()
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        resp = response.read()
 
-print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".
-      format(type(resp), resp, resp.decode('utf-8')))
+    print("""Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".
+        format(type(resp), resp, resp.decode('utf-8')))
