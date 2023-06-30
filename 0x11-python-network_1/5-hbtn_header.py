ac9c8f7e3f9917d7e475dc0b94387d9f5b49d2e5
Eben <maradebenezer@gmail.com>
2023-06-30 14:41:44 +0100

    fixed url to take it from command line

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,7 +4,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io'
+    url = sys.argv[1]
     r = requests.get(url)
-
     print(r.headers.get('X-Request-Id'))
