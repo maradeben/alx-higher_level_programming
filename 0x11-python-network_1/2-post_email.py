ad4b53b1d52f0affad549f59f8e17184b18927ef
Eben <maradebenezer@gmail.com>
2023-06-30 14:15:29 +0100

    fixing 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     params = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(params).encode('ascii')
-    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen((req)) as response:
         resp = response.read()
