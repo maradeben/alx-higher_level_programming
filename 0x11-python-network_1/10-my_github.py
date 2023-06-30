28247b5a2376a20c3a1f28591928e1d1f869bcf8
Eben <maradebenezer@gmail.com>
2023-06-30 15:48:49 +0100

    fixing 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ take GitHub credentials, use GitHub API to display id """
 import requests
-from requests.auth import HTTPBasicAuth
 import sys
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = 'https://api.github.com/users/{}'.format(username)
+    url = 'https://api.github.com/user'
 
-    r = requests.get(url, auth=HTTPBasicAuth(username, password))
+    r = requests.get(url, auth=(username, password))
     if r.status_code == 200:
         print(r.json().get('id'))
     else:
