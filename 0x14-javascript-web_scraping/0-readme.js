a0ee21de2e191f77eb6bea8bde9b44ef18470e41
Eben <maradebenezer@gmail.com>
2023-07-26 01:31:47 +0100

    fixing 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,5 @@
 // read and print contents of file
 
 const fs = require('fs');
-const data = fs.readFileSync(process.argv[1], 'utf8');
+const data = fs.readFileSync(process.argv[2], 'utf8');
 console.log(data);
