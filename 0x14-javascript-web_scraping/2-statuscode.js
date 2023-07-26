c61ff98fffd2fa3503b588313ebae7f012bbaebc
Eben <maradebenezer@gmail.com>
2023-07-26 02:05:35 +0100

    added 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// display status code of a GET request
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET',
+};
+
+request(options, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
