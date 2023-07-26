5d1573f45b11b6e9f1d4e6929420530998702829
Eben <maradebenezer@gmail.com>
2023-07-26 03:40:08 +0100

    added 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// compute number of tasks completed by user id
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const dash = {};
+    for (let i = 0; i < todos.length; i++) {
+      const key = todos[i].userId.toString();
+      if (todos[i].completed) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
+  }
+});
