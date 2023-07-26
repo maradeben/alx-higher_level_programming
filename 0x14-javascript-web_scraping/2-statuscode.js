64fd8ea381712eb510c6640fbb76c371ec1f06fd
Eben <maradebenezer@gmail.com>
2023-07-26 02:25:09 +0100

    fixing 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -12,6 +12,6 @@ request(options, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
